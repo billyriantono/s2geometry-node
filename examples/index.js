07b6b5bab695acf9a6d7f983845861b05c191921
Patrick <patricklo@gmail.com>
2016-08-25 16:30:35 -0700

    support getCovering

--- examples/index.js
+++ examples/index.js
@@ -11,3 +11,21 @@ console.log(latlng.normalized().lng);
 
 var origin = new s2.S2CellId(new s2.S2LatLng(37.7879938 ,-122.4074374));
 console.dir(origin.parent(15).children());
+
+var los_angeles = {lat: 34.0522, long: 118.2437}
+var new_york = {lat: 40.7128, long: 74.0059}
+var latLngLA = new s2.S2LatLng(los_angeles.lat, los_angeles.long);
+var latLngNY = new s2.S2LatLng(new_york.lat, new_york.long);
+
+var latLngRect = new s2.S2LatLngRect(latLngLA.toPoint(), latLngNY.toPoint());
+
+var region = new s2.S2RegionCoverer();
+var minLevel = 5;
+var maxLevel = 10;
+var maxCells = 20;
+var levelMod = 2;
+var results = region.getCovering(latLngRect, minLevel, maxLevel, maxCells, levelMod);
+for(var i = 0; i < results.length; i++)
+{
+	console.log(results[i].id())
+}
